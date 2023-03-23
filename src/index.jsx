b58bbe2247ff7793e819028390bfc888d017fe6a
Wanyu Jiang <wanyu19930930@gmail.com>
2023-03-23 23:32:16 +0900

    workshop done

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+// class Hello extends React.Component {
+//   render() {
+//     const { name, age } = this.props;
+//     return <h1>Hi {name}, your age is {age}!!!</h1>;
+//   }
+// }
+
+// const Hello = (props) => {
+//   // const name = props.name;
+//   // const age = props.age;
+//   const { name, age } = props;
+
+//   return <h1>Hi {name}, your age is {age}!!!</h1>;
+// };
+
+const containerDiv = document.getElementById('root');
+const root = ReactDOM.createRoot(containerDiv);
+root.render(<App />);
